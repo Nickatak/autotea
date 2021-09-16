4a8faba6c3735fd15b01ca484c51e891584ee438
Nickatak <nickle87@gmail.com>
2021-09-15 22:55:06 -0700

    Write temperature reader tests

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 .PHONY: lint
 lint:
 	pipenv run black ./
+.PHONY: test
+test:
+	pipenv run pytest tests/
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,3 +1,29 @@
 import mock
 
-from autotea import TemperatureReader
+import pytest
+
+from autotea.reader import TemperatureReader
+
+
+@pytest.mark.parametrize(
+    "attr_name",
+    (
+        "base_dir",
+        "fail_loud",
+        "file_path",
+    ),
+)
+@mock.patch("os.system")
+@mock.patch("glob.glob", return_value=["test-dir"])
+def test_init_should_set_attributes(glob_mock, system_mock, attr_name):
+    reader = TemperatureReader()
+
+    assert hasattr(reader, attr_name)
+
+@mock.patch("os.system")
+@mock.patch("glob.glob", return_value=[])
+def test_init_should_raise_exc_if_no_devices(glob_mock, system_mock):
+    with pytest.raises(IOError) as e:
+        reader = TemperatureReader()
+
+    assert str(e.value) == "Could not locate device interface.  Please see the following link to make sure you've enbaled 1-wire interfaces: https://www.raspberrypi-spy.co.uk/2013/03/raspberry-pi-1-wire-digital-thermometer-sensor/."
